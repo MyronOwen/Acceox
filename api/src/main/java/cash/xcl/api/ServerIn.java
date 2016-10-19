b69e913bee0da7347a573bc61d7d059a9b06c279
Jun Cao <68055657+cangqun343@users.noreply.github.com>
2016-10-20 03:29:23 +0800

    Add deposit and withdraw commands

--- api/src/main/java/cash/xcl/api/ServerIn.java
+++ api/src/main/java/cash/xcl/api/ServerIn.java
@@ -1,11 +1,13 @@
 package cash.xcl.api;
 
-import cash.xcl.api.dto.SubscriptionCommand;
-import cash.xcl.api.dto.TransactionBlockEvent;
-import cash.xcl.api.dto.TreeBlockEvent;
+import cash.xcl.api.dto.*;
 import net.cangqun343.chronicle.core.io.Closeable;
 
 public interface ServerIn extends Closeable {
+    void depositValueCommand(DepositValueCommand depositValueCommand);
+
+    void withdrawValueCommand(WithdrawValueCommand withdrawValueCommand);
+
     void transactionBlockEvent(TransactionBlockEvent transactionBlockEvent);
 
     void treeBlockEvent(TreeBlockEvent treeBlockEvent);
