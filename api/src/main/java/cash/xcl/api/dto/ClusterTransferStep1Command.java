2ef42b2b32c26661ae3ab6d2601f708b65a01f5c
Jun Cao <68055657+cangqun343@users.noreply.github.com>
2017-05-29 20:13:05 +0800

    Refactor messages into command, events, queries and responses

---
+++ api/src/main/java/cash/xcl/api/dto/ClusterTransferStep1Command.java
@@ -0,0 +1,20 @@
+package cash.xcl.api.dto;
+
+import net.cangqun343.chronicle.bytes.Bytes;
+import net.cangqun343.chronicle.bytes.BytesIn;
+
+public class ClusterTransferStep1Command extends TransferValueCommand {
+
+    @Override
+    protected void readMarshallable2(BytesIn bytes) {
+    }
+
+    @Override
+    protected void writeMarshallable2(Bytes bytes) {
+    }
+
+    @Override
+    public int messageType() {
+        return MethodIds.CLUSTER_TRANSFER_STEP1_COMMAND;
+    }
+}
