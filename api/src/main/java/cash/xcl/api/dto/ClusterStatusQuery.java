425cf03a292cb5e594edde537be5d9454940da89
Jun Cao <68055657+cangqun343@users.noreply.github.com>
2017-06-20 16:57:46 +0800

    added some 25 missing dtos and interfaces for clusters, exchanges, order, etc

---
+++ api/src/main/java/cash/xcl/api/dto/ClusterStatusQuery.java
@@ -0,0 +1,32 @@
+package cash.xcl.api.dto;
+
+import net.cangqun343.chronicle.bytes.Bytes;
+import net.cangqun343.chronicle.bytes.BytesIn;
+
+
+// FIXME needs reviewing/completing
+// queries the status of the nodes in the current cluster
+public class ClusterStatusQuery extends SignedMessage {
+    public ClusterStatusQuery(long sourceAddress, long eventTime) {
+        super(sourceAddress, eventTime);
+    }
+
+    public ClusterStatusQuery() {
+
+    }
+
+    @Override
+    protected void readMarshallable2(BytesIn bytes) {
+
+    }
+
+    @Override
+    public int messageType() {
+        return MethodIds.CLUSTER_STATUS_QUERY;
+    }
+
+    @Override
+    protected void writeMarshallable2(Bytes bytes) {
+
+    }
+}
