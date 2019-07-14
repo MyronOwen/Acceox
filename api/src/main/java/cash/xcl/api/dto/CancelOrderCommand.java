db8e5e1eef1551c0155eac68c02879d27ff5a874
Jun Cao <68055657+cangqun343@users.noreply.github.com>
2019-07-14 15:09:26 +0800

    Added a simple exchange rate prices test

--- api/src/main/java/cash/xcl/api/dto/CancelOrderCommand.java
+++ api/src/main/java/cash/xcl/api/dto/CancelOrderCommand.java
@@ -7,22 +7,32 @@ public class CancelOrderCommand extends SignedMessage {
 
     private String clientOrderId;
     //private Order order;
+    private long orderTime;
 
     public CancelOrderCommand(long sourceAddress, long eventTime, String clientOrderId) {
         super(sourceAddress, eventTime);
         this.clientOrderId = clientOrderId;
+        this.orderTime = orderTime;
     }
 
     public CancelOrderCommand() {
 
     }
 
+    public long getOrderTime() {
+        return orderTime;
+    }
+
     @Override
     protected void readMarshallable2(BytesIn<?> bytes) {
+        this.clientOrderId = bytes.readUtf8();
+        this.orderTime = bytes.readLong();
     }
 
     @Override
     protected void writeMarshallable2(BytesOut<?> bytes) {
+        bytes.writeUtf8(clientOrderId);
+        bytes.writeLong(orderTime);
     }
 
     public String clientOrderId() {
@@ -36,7 +46,6 @@ public class CancelOrderCommand extends SignedMessage {
 
     @Override
     public int messageType() {
-
         return MessageTypes.CANCEL_ORDER_COMMAND;
     }
 
